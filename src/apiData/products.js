6fb456857c57e7c68a211f633b9e213b6f425667
John Roe Dannug <jr.dannug@ipay88.com.ph>
2025-04-20 06:01:24 +0800

    new version 0549

--- src/apiData/products.js
+++ src/apiData/products.js
@@ -3,14 +3,14 @@
 import axios from 'axios';
 
 // Define the endpoint URL
-const baseLocal = 'https://localhost:7066';
+//const baseLocal = 'https://localhost:7066';
 const baseProd = 'https://mobileeasyshop.onrender.com';
 const getProductsUrl = '/api/Product/GetAllProduct';
 
 // Function to get products by categories
 export async function getProducts() {
   try {
-    const response = await axios.get(`${baseLocal}${getProductsUrl}`);
+    const response = await axios.get(`${baseProd}${getProductsUrl}`);
 
     return response.data;
   } catch (error) {
